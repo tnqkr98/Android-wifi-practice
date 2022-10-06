070d14eb9683c47281b3e80cad075110dd6ebbcd
luke.park <luke.park@cupix.com>
2022-10-06 15:04:47 +0900

    update test

--- app/src/main/java/com/example/android_wifi/MainActivity.kt
+++ app/src/main/java/com/example/android_wifi/MainActivity.kt
@@ -9,15 +9,19 @@ import android.net.*
 import android.net.wifi.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import android.provider.Settings
 import android.util.Log
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Button
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
-import java.net.Socket
-import java.net.URL
+import okhttp3.Request
 
 // android 8.0 - 8.1 : getScanResults 에 ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION, CHANGE_WIFI_STATE 중 하나의 권한 필요
 // android 9.0 : startScan() 을 위해선 ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION 중 하나, CHANGE_WIFI_STATE 필요
@@ -35,17 +39,17 @@ import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var wifiManager:WifiManager
-    lateinit var connectivityManager:ConnectivityManager
-    private val wifiScanReceiver = object:BroadcastReceiver() {
+    lateinit var wifiManager: WifiManager
+    lateinit var connectivityManager: ConnectivityManager
+    private val wifiScanReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
-            val success:Boolean? = intent?.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED,false)
-            if(success!!){
+            val success: Boolean? =
+                intent?.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false)
+            if (success!!) {
                 scanSuccess()
-            }
-            else{
+            } else {
                 scanFailure()
-                Log.d("res","scanFailure2")
+                Log.d("res", "scanFailure2")
             }
         }
 
@@ -54,18 +58,23 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private var defaultNetworkCallback: ConnectivityManager.NetworkCallback? = null
+    private var iotNetwork: Network? = null
+    private var networkCallback: ConnectivityManager.NetworkCallback? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Settings.System.canWrite(applicationContext)
-
-
-        requestPermissions(arrayOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_WIFI_STATE,
-            Manifest.permission.CHANGE_WIFI_STATE,
-            Manifest.permission.ACCESS_COARSE_LOCATION),101)
+        requestPermissions(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ), 101
+        )
 
         wifiManager = getSystemService(Context.WIFI_SERVICE) as WifiManager
         connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
@@ -97,30 +106,44 @@ class MainActivity : AppCompatActivity() {
 
             //connectWifi()
 
-            val suggestionReceiver = object : BroadcastReceiver(){
+            val suggestionReceiver = object : BroadcastReceiver() {
                 override fun onReceive(context: Context?, intent: Intent?) {
-                    Log.d("cupix network","wifi suggestion receive")
-                    if(!intent?.action.equals(WifiManager.ACTION_WIFI_NETWORK_SUGGESTION_POST_CONNECTION)){
+                    Log.d("cupix network", "wifi suggestion receive ?? ???")
+                    if (!intent?.action.equals(WifiManager.ACTION_WIFI_NETWORK_SUGGESTION_POST_CONNECTION)) {
                         return
                     }
                     //connectWifi()
                 }
             }
-            registerReceiver(suggestionReceiver,IntentFilter(WifiManager.ACTION_WIFI_NETWORK_SUGGESTION_POST_CONNECTION))
-
+            registerReceiver(
+                suggestionReceiver,
+                IntentFilter(WifiManager.ACTION_WIFI_NETWORK_SUGGESTION_POST_CONNECTION)
+            )
 
             val status = wifiManager.addNetworkSuggestions(suggestionList)
-            if(status != WifiManager.STATUS_NETWORK_SUGGESTIONS_SUCCESS){
+            if (status != WifiManager.STATUS_NETWORK_SUGGESTIONS_SUCCESS) {
                 //error
-                Log.d("cupix network","wifi suggestion error")
+                Log.d("cupix network", "wifi suggestion error")
             }
-            Log.d("cupix network","suggestion : success")
+            Log.d("cupix network", "suggestion : success")
         }
-
 
         val btn3 = findViewById<Button>(R.id.button3)
         btn3.setOnClickListener {
             connectWifi()
+        }
+
+        val btn5 = findViewById<Button>(R.id.button5)
+        btn5.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val client = OkHttpClient()
+                val request = Request.Builder()
+                    .url("http://192.168.1.1:80/osc/info")
+                    .build()
+
+                val response = client.newCall(request).execute()
+                response.body?.let { it1 -> Log.d("cupix network", it1.string()) }
+            }
         }
 
         val webView = findViewById<WebView>(R.id.web_view)
@@ -133,26 +156,60 @@ class MainActivity : AppCompatActivity() {
         btn4.setOnClickListener {
             webView.loadUrl("https://naver.com")
         }
+
+        // 명시적으로 바인딩되지 않아있으면 null 반환
+        Log.d("wifi network", "bound network : ${connectivityManager.boundNetworkForProcess}")
+
+        defaultNetworkCallback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                Log.d("android wifi", "default onAvailable")
+            }
+
+            override fun onLost(network: Network) {
+                Log.d("android wifi", "default onLost")
+                super.onLost(network)
+                //connectivityManager.unregisterNetworkCallback(this)
+            }
+
+            override fun onUnavailable() {
+                Log.d("android wifi", "default onAvailable")
+                super.onUnavailable()
+            }
+        }
+
+        connectivityManager.registerDefaultNetworkCallback(defaultNetworkCallback!!)
+
+
+        val btn6 = findViewById<Button>(R.id.button6)
+        btn6.setOnClickListener {
+            requestCellular()
+            //connectivityManager.bindProcessToNetwork(null)
+            //connectivityManager.unregisterNetworkCallback(defaultNetworkCallback!!)
+        }
     }
 
-    private fun wifiScan(){
+    private fun wifiScan() {
         val success = wifiManager.startScan()
-        if(!success){
-            Log.d("res","scanFailure1")
+        if (!success) {
+            Log.d("res", "scanFailure1")
             scanFailure()
         }
     }
 
-    private fun scanSuccess(){
+    private fun scanSuccess() {
         val results = wifiManager.scanResults
-        for(res in results){
-            Log.d("res","SSID : ${res.SSID}, BSSID : ${res.BSSID}, capabilities : ${res.capabilities}")
+        for (res in results) {
+            Log.d(
+                "res",
+                "SSID : ${res.SSID}, BSSID : ${res.BSSID}, capabilities : ${res.capabilities}"
+            )
         }
         // 새 스캔 결과 목록
     }
 
     private fun scanFailure() {
-        Log.d("res","scanFailure")
+        Log.d("res", "scanFailure")
         val results = wifiManager.scanResults
         // 이전 스캔 결과 목록
     }
@@ -161,71 +218,85 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
     }
 
-    private fun connectWifi(){
-        Log.d("cupix network","${wifiManager.wifiState}")
-        Log.d("cupix network","${wifiManager.isWifiEnabled}")
+    private fun connectWifi() {
+        Log.d("cupix network", "${wifiManager.wifiState}")
+        Log.d("cupix network", "${wifiManager.isWifiEnabled}")
 
-        val specifier = WifiNetworkSpecifier.Builder()
-            .setSsid("THETAYN12100323.OSC")
-            //.setBssid(MacAddress.fromString("C0:84:7D:F3:A3:E6"))
-            .setWpa2Passphrase("12100323")
-            //.setSsid("Cupix")
-            //.setBssid(MacAddress.fromString("5a:86:94:40:5e:94"))
-            //.setWpa2Passphrase("ScanRoom3D")
+        CoroutineScope(Dispatchers.IO).launch {
+            val handler = Handler(Looper.getMainLooper())
+
+            val specifier = WifiNetworkSpecifier.Builder()
+                .setSsid("THETAYN12100323.OSC")
+                .setWpa2Passphrase("12100323")
+                .build()
+
+            val request = NetworkRequest.Builder()
+                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+                .removeCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                .setNetworkSpecifier(specifier)
+                .build()
+
+
+            networkCallback = object : ConnectivityManager.NetworkCallback() {
+
+                override fun onAvailable(network: Network) {
+                    super.onAvailable(network)
+                    Log.d("android wifi", "wifi camera onAvailable")
+                    connectivityManager.bindProcessToNetwork(network)
+
+                    iotNetwork = network
+
+                }
+
+                override fun onUnavailable() {
+                    Log.d("android wifi", "wifi camera onUnavailable")
+                    super.onUnavailable()
+
+                    iotNetwork = null
+
+                    connectivityManager.unregisterNetworkCallback(this)
+                    connectWifi()
+                }
+
+                override fun onLost(network: Network) {
+                    Log.d("android wifi", "wifi camera onLost")
+                    super.onLost(network)
+                    connectivityManager.unregisterNetworkCallback(this)
+                }
+            }
+
+            connectivityManager.requestNetwork(request, networkCallback!!, handler, 10000)
+        }
+    }
+
+    private fun requestCellular() {
+        val networkRequest = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_NOT_METERED)
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
             .build()
 
+        connectivityManager.requestNetwork(
+            networkRequest,
+            object : ConnectivityManager.NetworkCallback() {
+                override fun onAvailable(network: Network) {
+                    super.onAvailable(network)
+                    //Log.d("android wifi", "cellular ${network}")
+                    Log.d("android wifi", "cellular onAvailable")
+                    connectivityManager.bindProcessToNetwork(network)
+                }
 
-        // [WPA2-PSK-CCMP][RSN-PSK-CCMP][ESS][WPS]
-        val wifiRequest = NetworkRequest.Builder()
-            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
-            .removeCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-            .setNetworkSpecifier(specifier)
-            .build()
+                override fun onLost(network: Network) {
+                    Log.d("android wifi", "cellular onLost")
+                    super.onLost(network)
+                    connectivityManager.unregisterNetworkCallback(this)
+                }
 
-        connectivityManager.requestNetwork(wifiRequest,object : ConnectivityManager.NetworkCallback() {
-            override fun onAvailable(network: Network) {
-                super.onAvailable(network)
-                Log.d("wifi network", "onAvailable $network")
-                //var netInfo = connectivityManager.activeNetwork
-
-                //var client = OkHttpClient()
-
-                //val success = connectivityManager.bindProcessToNetwork(network)
-                //network.bindSocket(Socket("http://192.168.1.1",80))
-                //Log.d("cupix network","success : $success")
-                //network.openConnection(URL("http://192.168.1.1:80"))
+                override fun onUnavailable() {
+                    Log.d("android wifi", "cellular onAvailable")
+                    super.onUnavailable()
+                }
             }
+        )
 
-            override fun onLosing(network: Network, maxMsToLive: Int) {
-                super.onLosing(network, maxMsToLive)
-                //Log.d("wifi network", "onLosing")
-            }
-
-            override fun onLost(network: Network) {
-                super.onLost(network)
-                Log.d("wifi network", "onLost")
-                connectivityManager.unregisterNetworkCallback(this)
-            }
-
-            override fun onUnavailable() {
-                super.onUnavailable()
-                //Log.d("wifi network", "onUnavailable")
-            }
-
-            override fun onCapabilitiesChanged(network: Network, networkCapabilities: NetworkCapabilities) {
-                super.onCapabilitiesChanged(network, networkCapabilities)
-                //Log.d("wifi network", "onCapabilitiesChanged , network : $network, networkCapabilities : $networkCapabilities")
-            }
-
-            override fun onLinkPropertiesChanged(network: Network, linkProperties: LinkProperties) {
-                super.onLinkPropertiesChanged(network, linkProperties)
-                //Log.d("wifi network", "onLinkPropertiesChanged network : $network, linkProperties : $linkProperties")
-            }
-
-            override fun onBlockedStatusChanged(network: Network, blocked: Boolean) {
-                super.onBlockedStatusChanged(network, blocked)
-                //Log.d("wifi network", "onBlockedStatusChanged")
-            }
-        })
     }
 }
